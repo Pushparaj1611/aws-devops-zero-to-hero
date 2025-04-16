586e0b8b8783ffa4ed0205849ee6b85327d39e07
Pushparaj1611 <48977851+Pushparaj1611@users.noreply.github.com>
2025-04-16 17:46:26 +0000

    updated

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull abhishekf5/simple-python-flask-app
+docker pull docker push 390402549997.dkr.ecr.us-west-2.amazonaws.com/sample-app:latest
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 abhishekf5/simple-python-flask-app
+docker run -d -p 5000:5000 390402549997.dkr.ecr.us-west-2.amazonaws.com/sample-app:latest
