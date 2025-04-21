6ef048bf9155ff1e6cbf61e8f38868f76e717c60
Pushparaj1611 <48977851+Pushparaj1611@users.noreply.github.com>
2025-04-22 00:56:07 +0530

    Update stop_container.sh

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 set -e
 
-
 # Stop and remove the most recent running container
 docker stop $(docker ps -q | head -n 1) && docker rm $(docker ps -q -f "status=exited" | head -n 1) && echo "Container stopped and removed."
