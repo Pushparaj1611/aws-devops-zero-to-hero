f2f344948a4c452e132547b45ed80a78bb698042
Pushparaj1611 <48977851+Pushparaj1611@users.noreply.github.com>
2025-04-22 00:03:22 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 set -e
-# login
-aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 306856845400.dkr.ecr.us-east-1.amazonaws.com
+
 # Pull the Docker image from Docker Hub
-docker pull 306856845400.dkr.ecr.us-east-1.amazonaws.com/sample-app:latest
+docker pull abhishekf5/simple-python-flask-app
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 306856845400.dkr.ecr.us-east-1.amazonaws.com/sample-app:latest
+docker run -d -p 5000:5000 abhishekf5/simple-python-flask-app
