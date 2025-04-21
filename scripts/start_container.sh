9607fc62b2af8d422a00806b94f7ca68a08d32da
Pushparaj1611 <48977851+Pushparaj1611@users.noreply.github.com>
2025-04-21 23:59:14 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 set -e
-
+# login
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 306856845400.dkr.ecr.us-east-1.amazonaws.com
 # Pull the Docker image from Docker Hub
-docker pull docker push 390402549997.dkr.ecr.us-west-2.amazonaws.com/sample-app:latest
+docker pull 306856845400.dkr.ecr.us-east-1.amazonaws.com/sample-app:latest
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 390402549997.dkr.ecr.us-west-2.amazonaws.com/sample-app:latest
+docker run -d -p 5000:5000 306856845400.dkr.ecr.us-east-1.amazonaws.com/sample-app:latest
