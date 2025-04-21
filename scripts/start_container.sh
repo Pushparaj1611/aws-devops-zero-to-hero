7c791d82723baed38fe37a796dcd65655dc80df7
Pushparaj1611 <48977851+Pushparaj1611@users.noreply.github.com>
2025-04-22 00:19:11 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -1,16 +1,9 @@
 #!/bin/bash
 set -e
+docker --version
+ping -c 2 google.com || true
+# Pull the Docker image from Docker Hub
+docker pull abhishekf5/simple-python-flask-app
 
-IMAGE_NAME="abhishekf5/simple-python-flask-app"
-IMAGE_TAG="latest"
-IMAGE_URI="${IMAGE_NAME}:${IMAGE_TAG}"
-
-echo "Pulling Docker image: $IMAGE_URI"
-docker pull "$IMAGE_URI"
-
-echo "Stopping old container if exists..."
-docker stop flask-app || true
-docker rm flask-app || true
-
-echo "Running new container..."
-docker run -d --name flask-app -p 5000:5000 "$IMAGE_URI"
+# Run the Docker image as a container
+docker run -d -p 5000:5000 abhishekf5/simple-python-flask-app
